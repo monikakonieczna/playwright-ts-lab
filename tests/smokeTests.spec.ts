141190846a675ff20a3e374a1947d9240027c750
MonikaKonieczna <konieczna.monika01@gmail.com>
2024-11-16 21:09:22 +0100

    test: Updating smoke tests

--- tests/smokeTests.spec.ts
+++ tests/smokeTests.spec.ts
@@ -1,4 +1,4 @@
-import { test } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
 /**
  * This 'beforeEach' hook navigates to the homepage
@@ -22,9 +22,13 @@ test.describe('Forms Smoke Suite', () => {
   )
   test('Should load the Form Layouts successfully.', async ({ page }) => {
     await page.getByText('Form Layouts').click();
+    // Assert that the correct URL was opened
+    await expect(page).toHaveURL(/.*\/pages\/forms\/layouts/);
   });
   test('Should load the Datepicker successfully.', async ({ page }) => {
     await page.getByText('Datepicker').click();
+    // Assert that the correct URL was opened
+    await expect(page).toHaveURL(/.*\/pages\/forms\/datepicker/);
   });
 
 })
@@ -42,9 +46,13 @@ test.describe('Tables & Data Smoke Suite', () => {
   )
   test('Should load the Smart Table successfully.', async ({ page }) => {
     await page.getByText('Smart Table').click();
+    // Assert that the correct URL was opened
+    await expect(page).toHaveURL(/.*\/pages\/tables\/smart-table/);
   });
   test('Should load the Tree Grid successfully.', async ({ page }) => {
     await page.getByText('Tree Grid').click();
+    // Assert that the correct URL was opened
+    await expect(page).toHaveURL(/.*\/pages\/tables\/tree-grid/);
   });
 
 })
