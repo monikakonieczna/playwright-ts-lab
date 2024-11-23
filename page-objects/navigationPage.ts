66fada6252ac9ae30e1e51c1407b40b1a8428ec7
MonikaKonieczna <konieczna.monika01@gmail.com>
2024-11-23 22:20:25 +0100

    refactor: Update locators in Navigation Page

--- page-objects/navigationPage.ts
+++ page-objects/navigationPage.ts
@@ -1,35 +1,46 @@
-import { Page } from '@playwright/test';
+import { Locator, Page } from '@playwright/test';
 
 export class NavigationPage {
     readonly page: Page;
+    readonly formLayoutsMenuItem: Locator;
+    readonly datePickerMenuItem: Locator;
+    readonly smartTableMenuItem: Locator;
+    readonly toastrMenuItem: Locator;
+    readonly tooltipMenuItem: Locator;
+
 
     constructor(page: Page) {
         this.page = page;
+        this.formLayoutsMenuItem = page.getByText('Form Layouts');
+        this.datePickerMenuItem = page.getByText('Datepicker');
+        this.smartTableMenuItem = page.getByText('Smart Table');
+        this.tooltipMenuItem = page.getByText('Tooltip');
+        this.toastrMenuItem = page.getByText('Toastr');
     }
 
     async formLayoutsPage() {
         await this.selectGroupMenuItem('Forms');
-        await this.page.getByText('Form Layouts').click();
+        await this.formLayoutsMenuItem.click();
     }
 
     async datePickerPage() {
         await this.selectGroupMenuItem('Forms');
-        await this.page.getByText('Datepicker').click();
+        await this.datePickerMenuItem.click();
     }
 
     async smartTablePage() {
         await this.selectGroupMenuItem('Table & Data');
-        await this.page.getByText('Smart Table').click();
+        await this.smartTableMenuItem.click();
     }
 
     async tooltipPage() {
         await this.selectGroupMenuItem('Modal & Overlays');
-        await this.page.getByText('Tooltip').click();
+        await this.tooltipMenuItem.click();
     }
 
     async toastrPage() {
         await this.selectGroupMenuItem('Modal & Overlays');
-        await this.page.getByText('Toastr').click();
+        await this.toastrMenuItem.click();
     }
 
     /**
@@ -39,7 +50,7 @@ export class NavigationPage {
     private async selectGroupMenuItem(groupItemTitle: string) {
         const groupMenuItem = this.page.getByTitle(groupItemTitle);
         const expandedStatus = await groupMenuItem.getAttribute('aria-expanded');
-        if (expandedStatus == "false") {
+        if (expandedStatus == 'false') {
             await groupMenuItem.click();
         }
     }
